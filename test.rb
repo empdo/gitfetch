fdb282bae77cb48ac2956608530ae2cca4dd8669
Emil Essung <emil.essung@live.se>
2021-05-30 00:51:46 +0200

    finished a bit more

--- test.rb
+++ test.rb
@@ -1,30 +1,42 @@
 require 'rugged' 
 require 'linguist'  
 require 'git'
+require 'json'
 
-#repo = Rugged::Repository.new('.')
-#project = Linguist::Repository.new(repo, repo.head.target_id)
-#print(project.language + "\n")
+file = File.read('icons.json')
+data_hash = JSON.parse(file)
+
+repo = Rugged::Repository.new('.')
+project = Linguist::Repository.new(repo, repo.head.target_id)
+lang = project.language.downcase
+
+unless data_hash["icons"].to_s.include? lang
+  lang = "others"
+end
 
 g = Git.open(Dir.pwd)
 
 lines = []
-File.foreach("js.txt").with_index do |line|
+data_hash["icons"][lang].each { |line|
   lines.append(line)
-end
+}
 
-stuff = [g.config('remote.origin.url').delete_prefix("git@github.com:"), 
+stuff = [g.config('remote.origin.url').delete_prefix("git@github.com:").gsub("/", "@"),
           "-----------", 
-          "Contributors: " + g.config("user.name"), 
+          "Author: " + g.config("user.name").to_s, 
           "Commits: " + (g.log.each{|x| x.to_s}).length.to_s,
-          "Branch: " + g.branch.to_s
+          "Branches: " + g.branch.to_s,
+          "Created: " + g.log.first.date.to_s[0, 10],
+          "About: i do not know how to get about text"
 ]
+
 
 i = 0
 stuff.each { |item|
   lines[i] = lines[i].chomp + item.chomp
   i += 1
 }
+
 
 lines.each { |line|
   line.chomp!
