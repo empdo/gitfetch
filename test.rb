f8c89d10db37218b1e284abba674344d183e9ae5
Emil Essung <emil.essung@live.se>
2021-05-29 18:00:52 +0200

    add start

--- test.rb
+++ test.rb
@@ -1,14 +1,38 @@
 require 'rugged'
 require 'linguist'
+require 'git'
+require 'logger'
 
-repo = Rugged::Repository.new('.')
-project = Linguist::Repository.new(repo, repo.head.target_id)
-print(project.language + "\n")
+#repo = Rugged::Repository.new('.')
+#project = Linguist::Repository.new(repo, repo.head.target_id)
+#print(project.language + "\n")
 
-file = File.open("js.txt")
-file_data = file.read  
-lines = file_data = file.readlines.map(&:chomp)
+working_dir = "/home/emil/dev/gitfetch/"
+g = Git.open(working_dir, :log => Logger.new(STDOUT))
 
-print(lines)
+lines = File.read("js.txt").split
+stuff =  [g.config('remote.origin.url').delete_prefix("git@github.com:") , "-----------", "Contributors:" + g.config("user.name"), g.log]
+
+
+stuff.each { |item|
+  puts(item)
+}
 
 #https://github.com/ruby-git/ruby-git
+#
+##    
+#    ████████████████████████     empdo@typeracer
+#    ████████████████████████     ---------------
+#    ████████████████████████     Contributors: empdo, alvesvaren
+#    ████████████████████████     Commits: 163
+#    ████████████████████████     Last commit: 21h 
+#    ██████████  ████    ████     Braches: main
+#    ██████████  ██  ████████     Created: 8/31 - 2020
+#    ██████████  ████    ████     About: Half done, shit website with shit code
+#    ██████████  ████████  ██
+#    ██████████  ██  ████  ██
+#    ██████    ██████    ████
+#    ████████████████████████
+
+
+
